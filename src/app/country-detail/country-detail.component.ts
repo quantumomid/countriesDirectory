df334e5b36d4b7f6e737042323c9418f3734083d
quantumomid <omid.wakili@outlook.com>
2022-02-01 13:26:05 +0000

    retrieve country details and display in country detail component

--- src/app/country-detail/country-detail.component.ts
+++ src/app/country-detail/country-detail.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-country-detail',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CountryDetailComponent implements OnInit {
 
-  constructor() { }
+  country: string;
+  data: any;
+  readonly ROOT_URL = "https://restcountries.com/v2/name/";
+
+  constructor(private route: ActivatedRoute, private http: HttpClient) {
+    this.country = route.snapshot.params["countryName"];
+  }
 
   ngOnInit(): void {
+    this.data = this.http.get(this.ROOT_URL+this.country);
   }
 
 }
