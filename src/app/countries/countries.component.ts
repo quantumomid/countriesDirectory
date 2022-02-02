311b07456fde4274bbf3ef314f13b4a7725dac71
quantumomid <omid.wakili@outlook.com>
2022-02-02 15:24:24 +0000

    add logic for filter pipe and use with the input for filtering countries by name

--- src/app/countries/countries.component.ts
+++ src/app/countries/countries.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-countries',
   templateUrl: './countries.component.html',
-  styleUrls: ['./countries.component.scss']
+  styleUrls: ['./countries.component.scss'],
 })
 export class CountriesComponent implements OnInit {
   inputTerm: string = "";
--- src/app/filter.pipe.ts
+++ src/app/filter.pipe.ts
@@ -4,9 +4,13 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'filter'
 })
 export class FilterPipe implements PipeTransform {
+  // the first parameter is what the pipe is being actually applied to - i.e. 
+  // when we use actual pipe therefore no need to actually pass the first parameter
+  transform(countries: any, inputTerm: any): any {
+    // check if search term is undefined
+    if (inputTerm === undefined) return countries;
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+    return countries.filter((country: any) => country.name.toLowerCase().includes(inputTerm.toLowerCase()));
   }
 
 }
